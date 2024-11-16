b7ca5b93eab7681ceaaa38ab31083849d8dbd46d
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-11-16 17:43:45 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,7 @@ const DynamicContent = dynamic(() => import("@/app/components/Content"), {
 });
 
 function Home() {
-  useEffect(() => {
-    // Dynamically set the height of the body to match the viewport
-    document.body.style.height = `${window.innerHeight}px`;
-  
-    return () => {
-      document.body.style.height = ""; // Reset on cleanup
-    };
-  }, []);
+
   
   return (
     <>
